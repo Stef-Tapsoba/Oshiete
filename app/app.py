64ed6f8538448c8f8f5a0d39cb79f79ccfa6d3af
Steffi Tapsoba <set89@drexel.edu>
2022-03-10 18:57:26 -0500

    basic error handling + some text edits

--- app/app.py
+++ app/app.py
@@ -8,7 +8,7 @@ import re
 app = Flask(__name__)
 app.secret_key = "hellokitty"
 
-#initialize mysql database
+#initialize mysql with info600 database
 mydb = mysql.connector.connect(
     host="127.0.0.1", #db
     port="33065",
@@ -16,7 +16,6 @@ mydb = mysql.connector.connect(
     passwd="example",
     database="info600"
 )
-
 
 #Home page
 @app.route("/", methods=["POST", "GET"])
@@ -38,14 +37,23 @@ def sign_up():
             email = str(userDetails['inputEmail'])
             password = str(userDetails['inputPassword'])
             
-            #Insert new user to database
+            #Check if user in Database and authenticate
             my_cursor = mydb.cursor()
-            my_cursor.execute("INSERT INTO users(username, email, password) VALUES(%s, %s, %s)", 
-                              (username, email, password))
-            mydb.commit()
+            my_cursor.execute("SELECT * FROM users")
+            records = my_cursor.fetchall()
             my_cursor.close()
-            flash("Your account has been successfully created! Log in now to start your journey!", "success")
-            return redirect(url_for('login'))
+            if (username, email, password) in records:
+                flash("You are already a user, you can just log in!", "success")
+                return redirect(url_for('login'))
+            else:
+            #Insert new user to database
+                my_cursor = mydb.cursor()
+                my_cursor.execute("INSERT INTO users(username, email, password) VALUES(%s, %s, %s)", 
+                                (username, email, password))
+                mydb.commit()
+                my_cursor.close()
+                flash("Your account has been successfully created! Log in now to start your journey!", "success")
+                return redirect(url_for('login'))
             
     except mysql.connector.Error as error:
         print("Failed to insert record")
@@ -53,15 +61,38 @@ def sign_up():
 
 
 #Sign in page with form t fill out
-@app.route("/login")
+@app.route("/login", methods=["POST", "GET"])
 def login():
+    try:
+        if request.method == 'POST':
+            #fetch form data
+            userDetails = request.form
+            username = str(userDetails['username'])
+            password = str(userDetails['password'])
+            
+            #Check if user in Database and authenticate
+            my_cursor = mydb.cursor()
+            my_cursor.execute("SELECT username, password FROM users")
+            records = my_cursor.fetchall()
+            my_cursor.close()
+            for record in records:
+                if record[0] == username and record[1] == password:
+                    my_cursor.close()
+                    return redirect(url_for('profile', user=username))
+            
+            flash("Incorrect username or password!", "error")
+            return redirect(url_for('login'))
+            
+    except mysql.connector.Error as error:
+        print("Failed to authenticate")
+        
     return render_template("login.html", title="Login")
 
 
 #User profile once they are logged in
-@app.route("/user/profile", methods=["POST", "GET"])
-def profile():
-    flash("Welcome back, "+str(request.form["username"]) +"! Ready to learn?")
+@app.route("/user/<user>-Profile", methods=["POST", "GET"])
+def profile(user):
+    flash("Welcome back, "+user +"! Ready to learn?", "success")
     return render_template("profile.html", title="Profile")
 
 
